df27736956fc6969c025c0e16844d9a4f48b9d21
nakachiF <nnntmargin681@gmail.com>
2021-10-22 01:26:54 +0900

    add 4 C files

---
+++ sample1_2.c
@@ -0,0 +1,18 @@
+// sample 1-2
+#include <stdio.h>
+int main()
+{
+    // int i = 7;
+    // float j = 10.5;
+    // char k = 'a';
+    // i = i / 3;
+    // j = j / 3;
+    // k = k + 'b';
+    // printf("i = %d \n", i);
+    // printf("j = %f \n", j);
+    // printf("k = %c \n", k);
+    for (int i = 9; i >= 0; i = i - 1)
+    {
+        printf("%d \n", i);
+    }
+}
